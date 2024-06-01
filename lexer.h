ee7b87ec696be1e1d12301db55fabc8d375fcab5
awkwardways <95001815+awkwardways@users.noreply.github.com>
2024-05-31 20:18:36 -0600

    Add files via upload
    Archivo de cabecera para analizador léxico :D

---
+++ lexer.h
@@ -0,0 +1,27 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+#define BUFFER_SIZE 4096
+#define MAX_LEXEME_LENGTH 256
+
+#include <stdio.h>
+typedef enum {
+    NUM = 256, ID, SEMICOLON, GREATER, LESSER, GREATEREQ, LESSEREQ, INCREMENT,
+    DECREMENT, PLUS, MINUS, ASSIGN, EQUAL, DIFFERENT, ADDASSIGN, SUBASSIGN, LGCL_NOT,
+    OP_PAR, CL_PAR, OP_CRLYBRACK, CL_CRLYBRACK, OP_SQREBRACK, CL_SQREBRACK, ENDOFFILE,
+    COMMA
+    } tokenTag;
+
+typedef struct
+{
+    tokenTag tag;
+    union 
+    {
+        char lexeme[MAX_LEXEME_LENGTH];
+        int numval;
+    }attribute;
+} token_t;
+
+void initLexer(FILE *fptr);
+token_t scan();
+#endif
